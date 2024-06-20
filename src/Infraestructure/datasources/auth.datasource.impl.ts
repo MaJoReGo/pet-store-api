3e7122107c7bb25aff52b906fed32f530d0d891a
Maria Jose Restrepo <restrepogomezmariajose@gmail.com>
2024-06-19 21:46:40 -0500

    Implementacion Manejo de errores en el controlador

--- src/Infraestructure/datasources/auth.datasource.impl.ts
+++ src/Infraestructure/datasources/auth.datasource.impl.ts
@@ -1,46 +1,57 @@
 import { BcryptAdapter } from "../../config";
-import { UserModel } from "../../data/mongodb/models/users.model";
+import { UserModel } from "../../data/mongodb/models/users.model"; //9
 import { UserMapper } from '../mappers/user.mapper';
-
 import {
   AuthDataSource,
   CustomError,
   RegisterUserDto,
   UserEntity,
-} from "../../domain"; 
+} from "../../domain"; //2
 
-type HashFunction = (password: string) => string;
+// creamos types de los metodos de encriptacion
+
+type HashFuntion = (password: string) => string;
+
 type CompareFunction = (password: string, hashed: string) => boolean;
 
 export class AuthDataSourceImpl implements AuthDataSource {
+  // inyeccion de dependencia para adapter de bcrypt
   constructor(
-    private readonly hashPassword: HashFunction = BcryptAdapter.hash,
+    private readonly hashPassword: HashFuntion = BcryptAdapter.hash,
     private readonly comparePassword: CompareFunction = BcryptAdapter.compare
   ) {}
 
+  //1
   async register(registerUserDto: RegisterUserDto): Promise<UserEntity> {
-    const { name, email, password } = registerUserDto; 
+    //3
+    const { name, email, password } = registerUserDto; // 4
 
     try {
+      //5
+
+      // verficar si el correo existe
       const exists = await UserModel.findOne({ email });
       if (exists) throw CustomError.badRequest("User already exists");
 
-      const hashedPassword = this.hashPassword(password);
+      // Crear el usuario
       const user = await UserModel.create({
         name: name,
         email: email,
-        password: hashedPassword,
+        password: this.hashPassword(password),
       });
-
+      // Guardamos el usuario en la base de datos
       await user.save();
 
-      // Mapear la respuesta a nuestra entidad
+      // TODO: Hash de contraseña
+
+      // TODO: Mapear la respuesta a nuestra entidad
       return UserMapper.userEntityFromObject(user);
     } catch (error) {
       if (error instanceof CustomError) {
+        //7
         throw error;
       }
-      throw CustomError.internalServer(); 
+      throw CustomError.internalServer(); //8
     }
   }
 }
